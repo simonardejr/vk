6932ac46b9e4f7dbfbaaa9c8bccea85d401a32dc
Simonarde Lima <simonarde@gmail.com>
2019-12-11 07:28:29 -0300

    rota/view: work in progress

--- app/Controllers/PageController.php
+++ app/Controllers/PageController.php
@@ -8,7 +8,13 @@ class PageController extends BaseController
     public function home()
     {
         // echo 'Hello World!';
-        $content = 'Hello World! ' . time();
+        $content = 'Hello World!';
         return $this->view('home', compact('content'));
+    }
+
+    public function about()
+    {
+        $content = 'Sobre';
+        return $this->view('about', compact('content'));
     }
 }
--- config/routes.php
+++ config/routes.php
@@ -2,6 +2,8 @@
 
 $router->get('', 'PageController@home');
 
+$router->get('about', 'PageController@about');
+
 $router->get('teste', function() {
     echo 'teste';
 });

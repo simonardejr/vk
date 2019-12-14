2706844f1f82945a1e9d813261b436ede56f80fc
Simonarde Lima <simonarde@gmail.com>
2019-12-14 18:57:41 -0300

    removendo linhas não utilizadas

--- vk/bootstrap.php
+++ vk/bootstrap.php
@@ -10,16 +10,10 @@ App::bind('request', new Request);
 // configurando o path das views para "/resources/views"
 App::bind('view_path', __DIR__ . '/../resources/views/');
 
-
-// $config = require '../config/database.php';
 App::bind('db_config', require '../config/database.php');
-
-// Connection::new($config['connections']['mysql']);
 
 App::bind('database', new QueryBuilder(
     Connection::new( 
         App::get('db_config')['connections']['mysql'] 
     )
 ));
-
-// var_dump(App::get('database')->selectAll('usuarios'));
